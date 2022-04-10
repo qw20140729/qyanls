dd80bf29a1c86cb999154dac6161aa85fbc2cf6f
qw20140729 <qw20140729@163.com>
2022-04-10 15:29:37 +0800

    提交学习总计

---
+++ lottery/myNum.py
@@ -0,0 +1,27 @@
+# -*-encoding:utf-8 -*-
+
+# File: myNum
+# Author: Administrator
+# CreateTime: 2022/4/10 11:05
+
+import random
+
+def getNum(num):
+    setn = set()
+    while True:
+        result = random.randint(1, num)
+        setn.add(result)
+        if len(setn) == 5 and num == 35:
+            break
+        if len(setn) == 2 and num == 12:
+            break
+    return setn
+
+def main():
+    x = [2, 11, 85, 37]
+    for i in range(100):
+        if i in x:
+            print(getNum(35), getNum(12))
+
+if __name__ == '__main__':
+    main()
