d3ea48419da778e8e415beca97c00d34b1ce202f
qyanls <qw20140729@163.com>
2022-06-19 23:57:56 +0800

    01

---
+++ usedScript/demo_connectionMssql.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# @File    : demo_connectionMssql.py
+# @Date    : 2022-06-19
+# @Author  : qyanls
+# Func：连接MSSQL数据库示例
+
+
+
+
+
+
---
+++ usedScript/demo_connectionMySql.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# @File    : demo_connectionMySql.py
+# @Date    : 2022-06-19
+# @Author  : qyanls
+# Func：连接mysql数据库
+
